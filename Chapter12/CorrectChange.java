9a13574637e053271c28d739b1a3d7bb97727df6
stegmanta <tae@myfreeapps.com>
2019-08-29 16:24:21 -0600

    complete CorrectChange

--- Chapter12/CorrectChange.java
+++ Chapter12/CorrectChange.java
@@ -12,17 +12,19 @@ public class CorrectChange{
         String inData = scan.nextLine();
         Integer totalChange = Integer.parseInt(inData);
 
-        double dollar;
-        double quarter;
-        double dime;
-        double nickle;
-        double penny;
-            dollar = totalChange/100;
-            quarter = (totalChange %100)/25;
-            dime = (totalChange %100)/10;
-            nickle = (totalChange%100)/5;
-            penny = (totalChange%100)/1;
-            System.out.println("The amount of dollars is " + dollar + " the amount of quarter "+ quarter + " the amount of dimes " + dime + "the amount of nickles"+ nickle + " the amount of pennies " + penny);
+        
+        
+           int dollar = totalChange/100;
+
+            int quarter = (totalChange - dollar * 100)/25;
+            
+            int dime = (totalChange - dollar * 100 - quarter * 25)/10;
+
+            int nickle = (totalChange - dollar * 100 - quarter *25 - dime * 10)/5;
+
+           int penny = (totalChange - dollar *100 - quarter*25 - dime *10 - nickle*5)/1;
+
+            System.out.println("The amount of dollars is " + dollar + " the amount of quarter "+ quarter + " the amount of dimes " + dime + " the amount of nickles "+ nickle + " the amount of pennies " + penny);
 
         
         
