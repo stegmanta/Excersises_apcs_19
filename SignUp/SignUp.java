9e6f5c5e4ad0a57924822786f63ba5b84f551945
Tayler Stegman <Tae@myfreeapps.com>
2019-09-03 12:53:00 -0600

    really stuck so goooddd luck :)

--- SignUp/SignUp.java
+++ SignUp/SignUp.java
@@ -3,6 +3,10 @@ public class SignUp{
 
     public static void main (String[] args){
 
+        boolean user1 = true;
+        boolean user2 = true;
+        boolean user3 = true;
+
         Scanner scanUsername = new Scanner(System.in);
         System.out.println("Submit a unique username");
 
@@ -14,8 +18,49 @@ public class SignUp{
         String takenUsername3 = ("admin person");
         String takenUsername4 = ("jimbob234");
 
-        if(inUserData.equals(takenUsername1,takenUsername2,takenUsername3,takenUsername4)){
+
+            if(inUserData.equals(takenUsername1)){
+                user1 = true;
+                System.out.println("Username taken");
+
+            }else{
+                user1 = false;
+                Scanner scanPassword = new Scanner(System.in);
+                System.out.println("User valid,  insert password");
+
+                String inPasswordData = scanPassword.nextLine();
+        }
+
+        if(!user1){
+            if(inUserData.equals(takenUsername2)){
+                user2 =true;
+                System.out.println("Username taken");
+
+
+            }else{
+                user2 = false;
+                Scanner scanPassword = new Scanner(System.in);
+                System.out.println("User valid,  insert password");
+
+                String inPasswordData = scanPassword.nextLine();
+            }
+        }
+
+        if(!user2){
+            if(inUserData.equals(takenUsername3)){
             System.out.println("Username taken");
+            user3 = true;
+            
+
+         }else{
+            user3 = false;
+            Scanner scanPassword = new Scanner(System.in);
+            System.out.println("User valid,  insert password");
+
+            String inPasswordData = scanPassword.nextLine();
+
+        }
+    }
 
 
         }
@@ -23,4 +68,3 @@ public class SignUp{
     }
 
 
-}
