01b3f442cbb058fb18fee051e0cce3b0fce4f648
Tayler <Tae@myfreeapps.com>
2019-09-08 20:03:13 -0600

    finish order

--- Chapter16/Order.java
+++ Chapter16/Order.java
@@ -13,8 +13,6 @@ public class Order{
 
         int bolts= totalBotls;
 
-
-
         System.out.println("Insert amount of nuts");
         
         String inNuts = amountofToolItems.nextLine();
@@ -27,27 +25,37 @@ public class Order{
         Integer totalWashers = Integer.parseInt(inWasher);
 
         int washers = totalWashers;
+        int totalCost = totalBotls*5+totalNuts*3+totalWashers;
        
 
-        if(nuts >= bolts){
+        if(nuts < bolts){
+            System.out.println("=======");
             System.out.println("Number of bolts: " + bolts);
             System.out.println("Number of nuts: " + nuts);
             System.out.println("Number of washers: " + washers);
-            System.out.println("Order is OK");
-        }else if(){
-            System.out.println("Number of bolts: " + bolts);
-            System.out.println("Number of nuts: " + nuts);
-            System.out.println("Number of washers: " + washers);
-            System.out.println("Check the Order: too few nuts");
-        }else if(2*washers<bolts){
-                System.out.println("Check the order: too few washers");
-                }else{
+            System.out.println("        ");
+            System.out.println("Check Order: too few nuts");
+            System.out.println("        ");
+            System.out.println("Total cost: " + totalCost );
+            }else if(2*bolts>washers){
+                System.out.println("=======");
                 System.out.println("Number of bolts: " + bolts);
                 System.out.println("Number of nuts: " + nuts);
                 System.out.println("Number of washers: " + washers);
-                System.out.println("Check the order: too few nuts");
-                System.out.println("Order is OK");
-                }
+                System.out.println("        ");
+                System.out.println("Check the order: too few washers");
+                System.out.println("        ");
+                System.out.println("Total cost: " + totalCost );
+                }else{
+                    System.out.println("=======");
+                    System.out.println("Number of bolts: " + bolts);
+                    System.out.println("Number of nuts: " + nuts);
+                    System.out.println("Number of washers: " + washers);
+                    System.out.println("        ");
+                    System.out.println("Order is OK");
+                    System.out.println("        ");
+                    System.out.println("Total cost: " + totalCost );
+                    }
 
 
 
