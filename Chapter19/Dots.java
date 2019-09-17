cd00cc4ced9feb6966c819cb7adc61704a4ab8f1
Tayler Stegman <Tae@myfreeapps.com>
2019-09-17 12:53:41 -0600

    complete sequential and start dots if u reading this alex or noah u dumb

---
+++ Chapter19/Dots.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Dots{
+
+    public static void main (String [] args){
+
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Enter first word:");
+        String firstWord = enter.nextLine();
+
+
+        System.out.println("Eneter second word:");
+        String secondWord = enter.nextLine();
+
+
+
+
+    }   
+}
--- Chapter19/Sequential.java
+++ Chapter19/Sequential.java
@@ -8,16 +8,17 @@ public class Sequential{
         System.out.println("Enter N:");
 
         Integer nValue = Integer.parseInt(user.nextLine());
-
+        int loopSum = 0;
         int one = 1;
         int sum = (nValue*(nValue+1))/2;
 
-        while(one<nValue)
+        while(one<nValue){
         one = one + 1;
         doneYet = false;
-        int loopSum = (nValue*(nValue+1))/2 +sum;
+        }
         if(!doneYet){
-            System.out.println("Sum = " + sum);
+            loopSum = (sum*nValue)/one;
+            System.out.println("Sum = " + sppum);
             System.out.println("Loop Sum = " + loopSum);
         }
 
