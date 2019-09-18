5d5fa048c7451c33b4882fe4773d6a0320220564
Tayler <Tae@myfreeapps.com>
2019-09-17 18:26:26 -0600

    finish dots ren if u reading this i dont care

--- Chapter19/Dots.java
+++ Chapter19/Dots.java
@@ -6,11 +6,28 @@ public class Dots{
         Scanner enter = new Scanner(System.in);
         System.out.println("Enter first word:");
         String firstWord = enter.nextLine();
-
+        int first = firstWord.length();
 
         System.out.println("Eneter second word:");
         String secondWord = enter.nextLine();
+        int second = secondWord.length();
 
+
+        int strTotalLength = second+first;
+        int totalLenght = 30-strTotalLength;
+        int amountODots = 0;
+
+        while(totalLenght>0){
+            totalLenght =totalLenght-1;
+            amountODots = amountODots +1;    
+        }
+
+        System.out.print(firstWord);
+        while(amountODots>0){
+            amountODots = amountODots-1;
+            System.out.print(".");
+        }
+        System.out.print(secondWord);
 
 
 
