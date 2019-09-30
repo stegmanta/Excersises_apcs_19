6988f073f28c2d07c0de1955e76b4667fe045bee
Tayler Stegman <Tae@myfreeapps.com>
2019-09-30 12:49:30 -0600

    almost finish sheep

--- Chapter24/Sheep.java
+++ Chapter24/Sheep.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Sheep{
 
     public static void main (String []args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is N :");
         
     }
 }
