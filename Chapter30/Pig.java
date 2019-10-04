a9ec06a8a61b89f56234880527d7e0b96f6c57b6
Tayler Stegman <Tae@myfreeapps.com>
2019-10-04 12:53:14 -0600

    worked on pig

--- Chapter30/Pig.java
+++ Chapter30/Pig.java
@@ -6,11 +6,23 @@ public class Pig{
 
         Random random = new Random();
         int cpuPoints = 0;
-        int dice = random.nextInt(18)+1;
-        int rollsRemaining = 3;
-        for(int playerPoints = 0; playerPoints<100 && rollsRemaining>0; rollsRemaining--){
-            playerPoints+=dice;
-            System.out.println("The player has rolled " + dice + " points");
+        int diceRoll = random.nextInt(18)+1;
+        int totalPlayerPoints = 0;
+
+
+        for(int playerPoints = 0,rollsRemaining = 3 ; playerPoints<100 && rollsRemaining>0; rollsRemaining--){
+
+            if(rollsRemaining==3){
+                totalPlayerPoints = playerPoints + diceRoll;
+                System.out.println("The player has rolled a " + diceRoll + "and has a total point of" + playerPoints);
+                diceRoll = random.nextInt(18)+1;
+                
+            }else if(rollsRemaining==2){
+                totalPlayerPoints = playerPoints +diceRoll;
+                
+                System.out.println("The player has rolled a " + diceRoll + " and has a total points of" + diceRoll);
+                
+            }
 
         }
     }
