9dd825d52a98e537623a3358d05c86a6a63aaad0
Tayler Stegman <Tae@myfreeapps.com>
2019-10-29 12:54:15 -0600

    start 47

---
+++ Chapter47/MoreCone.java
@@ -0,0 +1,12 @@
+public class MoreCone{
+    public static void main(String  []args){
+
+        private double height = 5;
+        private double radius = 3;
+        public double coneHeight(double height, double radius){
+
+        this.hei =5;
+        return this.hei;
+        }
+    }
+}
